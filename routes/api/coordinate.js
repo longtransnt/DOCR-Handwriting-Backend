e81bfb2a83eb87041f21f698bd5647b356757f3e
chrisidenbui <38187181+chrisidenbui@users.noreply.github.com>
2022-06-14 22:31:36 +0700

    get by id and post coordinate

--- routes/api/coordinate.js
+++ routes/api/coordinate.js
@@ -7,20 +7,28 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 const { v4: uuidv4 } = require('uuid');
 const aws = require("aws-sdk");
-const Coordinate = models.coordinate;
 
 
 router.get("/api/coordinate", function (req, res) {
-  Coordinate.findAll().then(coordinate => res.json(coordinate));
+  models.coordinate.findAll().then(coordinate => res.json(coordinate));
 });
 
-router.post("/api/coordinate", function (req, res) {
-  var body = req.body;
+router.get("/api/coordinate/:id", function (req, res) {
+  models.coordinate.findByPk(req.params.id).then(coordinate => res.json(coordinate));
+});
 
-  res.send (body);
-  // Coordinate.create({ coordinate: req.body.note, tag: req.body.tag }).then(function(note) {
-  //   res.json(note);
-  // });
+
+router.post("/api/coordinate", function (req, res) {
+  models.coordinate.create({
+    // image_id: DataTypes.UUID,
+    // original_image_id: DataTypes.UUID,
+    max_x: req.body.max_x,
+    max_y: req.body.max_y,
+    min_x:req.body.min_x,
+    min_y: req.body.min_y
+  }).then((c) => {
+    res.json(c);
+  });;
 });
 
 module.exports = router;
