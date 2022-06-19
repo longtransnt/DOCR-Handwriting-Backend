0d5c66de4dd2cd3888a05bd5a77137001f63763a
Long Tran <s3755614@rmit.edu.vn>
2022-06-19 19:56:49 +0700

    Test_upload_coordinate

--- routes/api/coordinate.js
+++ routes/api/coordinate.js
@@ -8,7 +8,6 @@ const upload = multer({ storage: storage });
 const { v4: uuidv4 } = require('uuid');
 const aws = require("aws-sdk");
 
-
 router.get("/api/coordinate", function (req, res) {
   models.coordinate.findAll().then(coordinate => res.json(coordinate));
 });
@@ -17,18 +16,16 @@ router.get("/api/coordinate/:id", function (req, res) {
   models.coordinate.findByPk(req.params.id).then(coordinate => res.json(coordinate));
 });
 
-
-router.post("/api/coordinate", function (req, res) {
-  models.coordinate.create({
-    // image_id: DataTypes.UUID,
-    // original_image_id: DataTypes.UUID,
-    max_x: req.body.max_x,
-    max_y: req.body.max_y,
-    min_x:req.body.min_x,
-    min_y: req.body.min_y
-  }).then((c) => {
-    res.json(c);
-  });;
+router.post("/api/coordinate", async (req, res) => {
+  await console.log(req.body);
+  // await models.coordinate.create({
+  //   image_name: req.body.image_name,
+  //   original_image_name: req.body.original_image_name,
+  //   max_x: req.body.max_x,
+  //   max_y: req.body.max_y,
+  //   min_x:req.body.min_x,
+  //   min_y: req.body.min_y
+  // });
 });
 
 module.exports = router;
